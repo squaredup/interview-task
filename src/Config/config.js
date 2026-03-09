3de41f78e6d317f6386893f7ca30aa81b8420b79
Tim Wheeler <63284593+TimWheeler-SQUP@users.noreply.github.com>
2026-03-09 14:24:33 +0000

    INT-001-Code-Alert update
    Trying different option to make GitHub trigger an alert

--- src/Config/config.js
+++ src/Config/config.js
@@ -1,4 +1,3 @@
-// This is a fake AWS Access Key and Secret Key for testing Secret Scanning.
-// GitHub will identify the pattern and flag it as a "Critical" alert.
-const AWS_ACCESS_KEY_ID = "AKIA1234567890EXAMPLE";
-const AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+// AWS Access Key and Secret Key for testing Secret Scanning.
+const AWS_ACCESS_KEY_ID = "AKIA12345678907890";
+const AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYRGR444";
